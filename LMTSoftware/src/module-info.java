90e46b47d3946c5f169b3c106135d41d8134a771
fabcasanova <54602317+fabcasanova@users.noreply.github.com>
2020-12-08 19:13:08 -0800

    Bare Bones Menu update
    Finished a unstyled starting menu with buttons.  Buttons changes to their respective scenes.

---
+++ LMTSoftware/src/module-info.java
@@ -0,0 +1,6 @@
+module LMTSoftware {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens phatsmalone.com.Main;
+}
---
+++ LMTSoftware/src/phatsmalone/com/Main/Controller.java
@@ -0,0 +1,41 @@
+package phatsmalone.com.Main;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+    Scene scene;
+    Stage window;
+
+@FXML
+private void NewProject(ActionEvent event) throws IOException {
+    Parent menuScene = FXMLLoader.load(getClass().getResource("NewProject.fxml"));
+    Scene newProjectScene = new Scene(menuScene);
+    window = (Stage)((Node)event.getSource()).getScene().getWindow();
+    window.setScene(newProjectScene);
+    window.show();
+};
+
+@FXML
+private void OpenProject(ActionEvent event) throws IOException{
+    Parent openScene = FXMLLoader.load(getClass().getResource("OpenProject.fxml"));
+    Scene oProjectScene = new Scene(openScene);
+    window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+    window.setScene(oProjectScene);
+    window.show();
+};
+
+@FXML
+private void ExitProject(ActionEvent event) throws IOException{
+    Platform.exit();
+};
+}
